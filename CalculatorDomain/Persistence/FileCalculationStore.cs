fc7dd35a51836a19250f56d2021104911188aac4
Zanke <zanke.ferreira1229@gmail.com>
2026-02-03 13:17:13 +0200

    Added soluntion files and classes

---
+++ CalculatorDomain/Persistence/FileCalculationStore.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using CalculatorDomainDemo.Domain;
+using CalculatorDomainDemo.Persistence;
+
+namespace CalculatorDomain.Persistence
+{
+    public class FileCalculationStore : ICalculationStore
+    {
+        private readonly string _filePath;
+
+        public FileCalculationStore(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public async Task SaveAsync(Calculation calculation)
+        {
+            var calculations = (await LoadAllAsync()).ToList();
+            calculations.Add(calculation);
+
+            var json = JsonSerializer.Serialize(calculations);
+            await File.WriteAllTextAsync(_filePath, json);
+        }
+
+        public async Task<IReadOnlyList<Calculation>> LoadAllAsync()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Calculation>();
+
+            string json = await File.ReadAllTextAsync(_filePath);
+
+            if (string.IsNullOrWhiteSpace(json))
+                return new List<Calculation>();
+
+            List<Calculation> calculations = JsonSerializer.Deserialize<List<Calculation>>(json)
+            ?? new List<Calculation>();
+
+            return calculations;
+        }
+    }
+}
